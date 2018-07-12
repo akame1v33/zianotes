f83d2eaf792cbe1b32da50b5ef3d2e03e6a89baa
Kevin Fabian <akame1v33@gmail.com>
2018-07-12 18:08:25 +0800

    Auto-committed on Thu 07/12/2018

--- commit.bat
+++ commit.bat
@@ -7,15 +7,7 @@ cd \springmvcnotes
 echo CURRENT LOCATION %cd%
 
 set GIT_PATH="C:\Users\kevin.fabian\AppData\Local\Programs\Git\bin\git.exe"
-
-:P
-set FUCK= 
-set /P FUCK=Action: %=%
-
-if "%FUCK%"=="commit" (
-    %GIT_PATH% add .
-	%GIT_PATH% commit -m "Auto-committed on %date%"
-	%GIT_PATH% push origin master
-)
-if "%FUCK%"=="exit" exit /b
-goto P
+%GIT_PATH% add .
+%GIT_PATH% commit -m "Auto-committed on %date%"
+%GIT_PATH% push origin master
+exit /b
