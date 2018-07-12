5bffb149947d748e1b531538d0360054bc7ded55
Kevin Fabian <akame1v33@gmail.com>
2018-07-12 17:42:50 +0800

    Auto-committed on Thu 07/12/2018

--- commit.bat
+++ commit.bat
@@ -1,3 +1,21 @@
-git add .
-git commit -m "AUTO COMMIT"
-git push
+@echo off
+echo type "commit" or "update"
+cd "curl"
+
+set GIT_PATH="C:\Users\kevin.fabian\AppData\Local\Programs\Git\bin\git.exe"
+set BRANCH = "origin"
+
+:P
+set ACTION=
+set /P ACTION=Action: %=%
+if "%ACTION%"=="c" (
+  %GIT_PATH% add -A
+	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% pull %BRANCH%
+	%GIT_PATH% push %BRANCH%
+)
+if "%ACTION%"=="u" (
+	%GIT_PATH% pull %BRANCH%
+)
+if "%ACTION%"=="exit" exit /b
+goto P
